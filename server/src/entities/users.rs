29436755e74cb4eaebea78594fe4dc2d4531c3df
Abdulqudduus Babalola <babalolajnr@gmail.com>
2023-04-24 15:24:17 +0100

    feat: add relationship

--- server/src/entities/users.rs
+++ server/src/entities/users.rs
@@ -18,6 +18,15 @@ pub struct Model {
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-pub enum Relation {}
+pub enum Relation {
+    #[sea_orm(has_many = "super::passwords::Entity")]
+    Passwords,
+}
+
+impl Related<super::passwords::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Passwords.def()
+    }
+}
 
 impl ActiveModelBehavior for ActiveModel {}
